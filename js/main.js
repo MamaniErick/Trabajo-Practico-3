54f66cb8c0062770da7bf7745d0fca91879ae99d
MamaniErick <mamanierick64@gmail.com>
2026-04-26 03:07:58 -0300

    Agregado de transicion a los Contadores

--- js/main.js
+++ js/main.js
@@ -3,14 +3,39 @@ $(document).ready(function () {
     // Ocultar elementos inicialmente
     $(".hero-title, .hero-subtitle, .btn-hero").hide();
 
-    // Secuencia de animaciones
-     $(".hero-title, .hero-subtitle, .btn-hero").hide();
+    // Animaciones
+    $(".hero-title, .hero-subtitle, .btn-hero").hide();
 
     $(".hero-title").fadeIn(1200);
 
     $(".hero-subtitle").delay(600).fadeIn(1200);
 
     $(".btn-hero").delay(1200).fadeIn(1200);
+
+    //FUNCION DE AMIMACION DE CONTADORES
+    function animarContador() {
+        $(".numero").each(function () {
+
+            let $this = $(this);
+            let target = parseInt($this.attr("data-target"));
+
+            $({ countNum: 0 }).animate({
+                countNum: target
+            }, {
+                duration: 3500,
+                easing: "swing",
+                step: function () {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                    $this.text(target + (target === 100 ? "%" : "+"));
+                }
+            });
+        });
+    }
+
+    // SIMULAR CUANDO CARGA LA PAGINA
+    animarContador();
 
     // DESTINOS
     // AGENCIAS
