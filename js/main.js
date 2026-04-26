ee2107c1d26899f003c1f4137d07267bd0125093
MamaniErick <mamanierick64@gmail.com>
2026-04-26 02:22:02 -0300

    Agregado de transicio al Hero

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
     // HOME
+    // Ocultar elementos inicialmente
+    $(".hero-title, .hero-subtitle, .btn-hero").hide();
+
+    // Secuencia de animaciones
+     $(".hero-title, .hero-subtitle, .btn-hero").hide();
+
+    $(".hero-title").fadeIn(1200);
+
+    $(".hero-subtitle").delay(600).fadeIn(1200);
+
+    $(".btn-hero").delay(1200).fadeIn(1200);
+
     // DESTINOS
     // AGENCIAS
     // CONTACTO
